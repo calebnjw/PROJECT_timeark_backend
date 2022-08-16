15ea78ac6b4ce40b7335dbaa8d25a56b81c5e81a
eva.wang <eva.wang81@gmail.com>
2022-08-16 16:32:10 +0200

    added controllers

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,16 +1,46 @@
-const BaseController = require("./baseController");
+const { Response: res, Request: req } = require("express");
 
-class TaskController extends BaseController {
-  constructor(model) {
-    super(model);
-    // this.userMongo = userMongo
-  }
+const getTasks = (req, res) => {
+  res.json({ msg: "all tasks" });
+};
 
-  async timeInsert(req, res) {
-    const newTask = await this.model.createOne({ ...req.body });
-    // const userCheck = await this.userMongo.findById(newUser._id)
-    return res.json({ newUser });
-  }
-}
+const getSingleTask = (req, res) => {
+  res.json({ msg: `show single task: ${req.params.id}` });
+};
 
-module.exports = TaskController;
+const addNewTask = (req, res) => {
+  res.json({ msg: "create a new task" });
+};
+
+const updateTask = (req, res) => {
+  res.json({ msg: `update task: ${req.params.id}` });
+};
+
+const deleteTask = (req, res) => {
+  res.json({ msg: `delete task: ${req.params.id}` });
+};
+
+module.exports = {
+  getTasks,
+  getSingleTask,
+  addNewTask,
+  updateTask,
+  deleteTask,
+};
+
+// const BaseController = require("./baseController");
+
+// class TaskController extends BaseController {
+//   constructor(model) {
+//     super(model);
+//     // this.userMongo = userMongo
+//   }
+
+//   async timeInsert(req, res) {
+//     const newTask = await this.model.createOne({ ...req.body });
+//     // const userCheck = await this.userMongo.findById(newUser._id)
+//     return res.json({ newUser });
+//   }
+// }
+
+// module.exports = TaskController;
--- src/routers/tasksRouter.js
+++ src/routers/tasksRouter.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { Response: res, Request: req } = require("express");
+const {
+  getTasks,
+  getSingleTask,
+  addNewTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasksController");
 
 router
-  .get("/", (req, res) => {
-    res.json({ msg: "all tasks" });
-  })
-  .get("/:id", (req, res) => {
-    res.json({ msg: `show single task: ${req.params.id}` });
-  })
-  .post("/new", (req, res) => {
-    res.json({ msg: "create a new task" });
-  })
-  .delete("/:id", (req, res) => {
-    res.json({ msg: `delete task: ${req.params.id}` });
-  });
+  .get("/", getTasks)
+  .get("/:id", getSingleTask)
+  .post("/new", addNewTask)
+  .put("/:id", updateTask)
+  .delete("/:id", deleteTask);
 
 module.exports = router;
+
 // class TasksRouter {
 //   constructor(controller) {
 //     this.controller = controller;
