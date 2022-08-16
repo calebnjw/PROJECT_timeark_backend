a2eb428b9f21e9a268c410b2036886780c917cca
eva.wang <eva.wang81@gmail.com>
2022-08-16 21:33:56 +0200

    added task controller

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -6,9 +6,30 @@ class TaskController {
     this.model = model;
   }
 
-  async addNewTask(req: Request, res: Response) {
+  async getAllTasks(req: Request, res: Response) {
+    const tasks = await this.model.findAll();
+    return res.json({ tasks });
+  }
+
+  async createTask(req: Request, res: Response) {
     const newTask = await this.model.create({ ...req.body });
     return res.json({ newTask });
+  }
+
+  async getSingleTask(req: Request, res: Response) {
+    const task = await this.model.findById(req.params.id);
+    return res.json({ task });
+  }
+
+  async updateTask(req: Request, res: Response) {
+    const task = await this.model.findById(req.params.id);
+    return res.json({ task });
+  }
+
+  async deleteTask(req: Request, res: Response) {
+    const task = await this.model.findById(req.params.id);
+    task.destroy();
+    return res.json({ task });
   }
 }
 
