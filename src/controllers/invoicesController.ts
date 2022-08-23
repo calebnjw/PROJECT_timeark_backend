782f6b0fa99625fa9fc121da879d935f6164a21c
nikkirosche <101503635+nikkirosche@users.noreply.github.com>
2022-08-23 22:42:35 +0800

    updated commit

--- src/controllers/invoicesController.ts
+++ src/controllers/invoicesController.ts
@@ -2,27 +2,12 @@ import { Request, Response } from "express";
 import { Model } from "mongoose";
 import IInvoices from "../interfaces/invoice";
 import Project from "../models/project";
-import Client from "../models/client";
 
 class InvoiceController {
   public model: Model<IInvoices>;
   constructor(model: Model<IInvoices>) {
     this.model = model;
   }
-  // async getAllProjects(req: Request, res: Response) {
-  //   try {
-  //     //am I doing this part right?
-  //     const { client_id } = req.query;
-  //     console.log("Client_id: ", client_id);
-  //     const client: any = await Client.findById(client_id).populate(
-  //       "project_ids"
-  //     );
-  //     const projects = client.project_ids;
-  //     return res.json({ projects });
-  //   } catch (err) {
-  //     console.log("Error message: ", err);
-  //   }
-  // }
   
     async getAllInvoices(req: Request, res: Response) {
       try {
