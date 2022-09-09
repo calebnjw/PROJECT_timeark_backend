174af01412f91c704093be64ecee0df7f2ead948
nikkirosche <101503635+nikkirosche@users.noreply.github.com>
2022-09-09 12:22:09 +0800

    updated commit

--- src/controllers/invoicesController.ts
+++ src/controllers/invoicesController.ts
@@ -17,15 +17,11 @@ class InvoiceController {
   async getAllInvoices(req: Request, res: Response) {
     try {
       const { project_id } = req.query;
-      console.log("Project id,getallinvoices: ", project_id);
       const project: any = await Project.findById(project_id).populate(
         "invoice_ids"
       );
-      console.log("Projectvariable", project)
 
       const invoices = project.invoice_ids;
-      console.log('invoices: ', invoices)
-      // return res.json({ invoices });
       return res.json({msg: "requested!", invoices })
     } catch (err) {
       console.log("Error message: ", err);
@@ -35,32 +31,29 @@ class InvoiceController {
   async createInvoice(req: Request, res: Response) {    
     try {
       const { project_id, selectedMonth} = req.body;
-      //get all tasks from the project id 
-      const project: any = await Project.findById(project_id)
-      console.log("project id", project_id)   
-      console.log("month", selectedMonth) 
-      console.log("projectcreatinvoice", project)
 
-      //info needed to create invoice as json file
-      const newInvoiceDetails = {
-        project_id,
-        month: selectedMonth,
-        paid: true,
-        overdue: false
+      const selectedProject: any = await Project.findById(project_id).populate(
+        "invoice_ids"
+      );
+
+      const issuedInvoices = selectedProject.invoice_ids;
+
+      const existingInvoice = issuedInvoices.find((i:any) => i.month === selectedMonth)
+      if(existingInvoice){
+        return res.json({msg: "Invoice already issued for the month selected"})
+      } else {
+        const newInvoice = {
+          project_id: project_id,
+          paid: false,
+          issuedDate: new Date(),
+          overdue: false,
+          month: selectedMonth
+        }
+        const newInvoiceDetails = await this.model.create(newInvoice);
+        selectedProject?.invoice_ids.push(newInvoiceDetails._id);
+        await selectedProject?.save()
+        return res.json({ msg: "Added new invoice" });
       }
-      const newInvoice = await this.model.create(newInvoiceDetails);
-      await newInvoice.save();
-      if (typeof project.invoices === "undefined"){
-        project.invoices=[]
-      }
-      if (typeof project.invoice_ids === "undefined"){
-        project.invoice_ids=[]
-      }
-      project.invoices.push(newInvoice)
-      project.invoice_ids.push(newInvoice._id)
-      await project.save()
-      console.log("newinvoicedetails", newInvoiceDetails)
-      return res.json({ msg: "Added new invoice" });
     } catch (err) {
       console.log("Error message: ", err);
       return res.json({ err: "Invoice not created" });
