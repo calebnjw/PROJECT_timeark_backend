e7cdd08cf2177046644c58f07c03f0542eeb53e1
eva.wang <eva.wang81@gmail.com>
2022-09-09 10:15:09 +0800

    refactored passport

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -10,6 +10,42 @@ const GOOGLE_CLIENT_SECRET: string = process.env.GOOGLE_CLIENT_SECRET;
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+let authUser = async (
+  request: Request,
+  accessToken: string,
+  refreshToken: string | undefined,
+  profile: Profile,
+  done: VerifyCallback
+) => {
+  const { emails } = profile; // get emails from google profile
+  let user: any; // declare variable to store emails
+
+  // if user profile has emails
+  if (emails && emails.length > 0) {
+    user = await UserModel.findOne({ "emails.value": emails[0].value }); // lookup database for account with same email
+  }
+  // if user returns null,
+  // create new entry in database
+  if (!user) {
+    user = await UserModel.create({
+      provider: profile.provider,
+      externalId: profile.id,
+      displayName: profile.displayName,
+      name: profile.name,
+      emails: profile.emails,
+      photos: profile.photos,
+      billingDetails: {},
+      clientIds: [],
+    });
+    console.log("creating new user");
+    user.newUser = true; // TODO: if new user is true, login page should redirect to billing info update page
+  } else {
+    user.newUser = false;
+  }
+  console.log("running done");
+  return done(null, user);
+};
+
 passport.use(
   new GoogleStrategy(
     // Google app credentials
@@ -17,55 +53,23 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: `http://localhost:${PORT}/auth/google/callback`,
+      passReqToCallback: true,
     },
-    // function that receives profile information from Google
-    // and passes user object to serializeUser
-    async (
-      request: Request,
-      accessToken: string,
-      refreshToken: string | undefined,
-      profile: Profile,
-      done: VerifyCallback
-    ) => {
-      const { emails } = profile; // get emails from google profile
-      let user: any; // declare variable to store emails
-
-      // if user profile has emails
-      if (emails && emails.length > 0) {
-        user = await UserModel.findOne({ "emails.value": emails[0].value }); // lookup database for account with same email
-      }
-
-      // if user returns null,
-      // create new entry in database
-      if (!user) {
-        user = await UserModel.create({
-          provider: profile.provider,
-          externalId: profile.id,
-          displayName: profile.displayName,
-          name: profile.name,
-          emails: profile.emails,
-          photos: profile.photos,
-          billingDetails: {},
-          clientIds: [],
-        });
-        user.newUser = true; // TODO: if new user is true, login page should redirect to billing info update page
-      } else {
-        user.newUser = false;
-      }
-
-      return done(null, user);
-    }
+    authUser
   )
 );
 
 // takes id, displayName and newUser state from previous return
 // and passes it to deserializeUser.
-passport.serializeUser((user: Express.User, done: VerifyCallback) => {
-  const { id, displayName, newUser } = user;
-  return done(null, { id, displayName, newUser });
+passport.serializeUser((userModel: globalThis.Express.User, done) => {
+  console.log("serializing user");
+  const { id, displayName, newUser } = userModel;
+  console.log("----> serialized userModel:", userModel);
+  done(null, { id, displayName, newUser });
 });
 
-// saves information from previous return into request.session.passport.user.
-passport.deserializeUser((user: Express.User, done: VerifyCallback) => {
-  return done(null, user);
+passport.deserializeUser((userModel: globalThis.Express.User, done) => {
+  console.log("Deserializing user");
+  console.log("----> deserialized userModel:", userModel);
+  done(null, userModel);
 });
