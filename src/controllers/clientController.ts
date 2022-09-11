8a6a3645d75d47e02e24d24f97c63dbe66293f78
Goh Yong Siang Ivan <37769720+gysiang@users.noreply.github.com>
2022-09-11 12:43:05 +0800

    added route to delete client

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import BSON from "BSON";
 import { Model, ObjectId } from "mongoose";
-
+import Project from "../models/project";
 import IClients from "../interfaces/client";
 import users from "../models/users";
 
@@ -34,7 +34,7 @@ class ClientController {
       const data = await this.model.find({ _id: new BSON.ObjectId(clientId) });
       return response.status(200).json(data);
     } catch (error) {
-      console.log(error);
+      return response.status(500).json("no client found!");
     }
   }
 
@@ -43,9 +43,9 @@ class ClientController {
 
     try {
       const data = await this.model.create(clientDetails);
-      return response.status(201).json(data);
+      return response.status(200).json(data);
     } catch (error) {
-      console.log(error);
+      return response.status(500).json("no client found!");
     }
   }
 
@@ -61,7 +61,20 @@ class ClientController {
       );
       return response.status(201).json(data);
     } catch (error) {
-      console.log(error);
+      return response.status(500).json("update client error");
+    }
+  }
+
+  async deleteClient(request: Request, response: Response) {
+    const { clientId } = request.params;
+    try {
+      // delete client from client table
+      await this.model.findByIdAndDelete(clientId);
+      // delete projects from with client id
+      await Project.deleteMany({ client_id: clientId });
+      return response.json("deleted client successfully");
+    } catch (error) {
+      console.log("Error message: ", error);
     }
   }
 }
