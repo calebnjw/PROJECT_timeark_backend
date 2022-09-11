17b120f811c3d4d9127a6779aaa1cbd35b2df131
eva.wang <eva.wang81@gmail.com>
2022-09-11 18:43:55 +0800

    Fix MongoDB connextion string and use profile.id as identifier for google login

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -21,29 +21,29 @@ let authUser = async (
   const { emails } = profile; // get emails from google profile
   let user: any; // declare variable to store emails
 
-  // if user profile has emails
-  if (emails && emails.length > 0) {
-    user = await UserModel.findOne({ "emails.value": emails[0].value }); // lookup database for account with same email
-  }
+  user = await UserModel.findOne({ externalId: profile.id }); // lookup database for account with same email
+
   // if user returns null,
   // create new entry in database
   if (!user) {
-    user = await UserModel.create({
-      provider: profile.provider,
-      externalId: profile.id,
-      displayName: profile.displayName,
-      name: profile.name,
-      emails: profile.emails,
-      photos: profile.photos,
-      billingDetails: {},
-      clientIds: [],
-    });
-    console.log("creating new user");
+    try {
+      user = await UserModel.create({
+        provider: profile.provider,
+        externalId: profile.id,
+        displayName: profile.displayName,
+        name: profile.name,
+        emails: profile.emails,
+        photos: profile.photos,
+        billingDetails: {},
+        clientIds: [],
+      });
+    } catch (error) {
+      console.log(error);
+    }
     user.newUser = true; // TODO: if new user is true, login page should redirect to billing info update page
   } else {
     user.newUser = false;
   }
-  console.log("running done");
   return done(null, user);
 };
 
@@ -63,14 +63,14 @@ passport.use(
 // takes id, displayName and newUser state from previous return
 // and passes it to deserializeUser.
 passport.serializeUser((userModel: globalThis.Express.User, done) => {
-  console.log("serializing user");
+  // console.log("serializing user");
   const { id, displayName, newUser } = userModel;
-  console.log("----> serialized userModel:", userModel);
-  done(null, { id, displayName, newUser });
+  // console.log("----> serialized userModel:", userModel);
+  done(null, { id: id, name: displayName, user: newUser });
 });
 
 passport.deserializeUser((userModel: globalThis.Express.User, done) => {
-  console.log("Deserializing user");
-  console.log("----> deserialized userModel:", userModel);
+  // console.log("Deserializing user");
+  // console.log("----> deserialized userModel:", userModel);
   done(null, userModel);
 });
