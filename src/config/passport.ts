cca03b05edd641add29ab3b67c7599a2b5f867bd
nikkirosche <101503635+nikkirosche@users.noreply.github.com>
2022-09-12 23:09:13 +0800

    route fix

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -66,7 +66,7 @@ passport.serializeUser((userModel: globalThis.Express.User, done) => {
   // console.log("serializing user");
   const { id, displayName, newUser } = userModel;
   // console.log("----> serialized userModel:", userModel);
-  done(null, { id: id, name: displayName, user: newUser });
+  done(null, { id: id, name: displayName, newUser: newUser });
 });
 
 passport.deserializeUser((userModel: globalThis.Express.User, done) => {
