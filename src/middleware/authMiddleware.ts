ee35f52c188c537e45017ef2ec25058a0c32d333
Caleb <5322420+calebnjw@users.noreply.github.com>
2022-09-17 01:15:18 +0800

    removed authentication middleware, not in use;

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,21 +2,14 @@ import { NextFunction, Request, Response } from "express";
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const isAuthenticated =
-  () => (request: Request, response: Response, next: NextFunction) => {
-    console.log("ISAUTHENTICATED MIDDLEWARE");
-    console.log(request.user);
-    if (request.user) {
-      console.log("REQUEST.USER EXISTS");
-      response.cookie("loggedIn", true);
-      return next();
-    }
-    // if user is not logged in, redirect.
-    console.log("NO REQUEST.USER, REDIRECTING TO LOGIN");
-    response.cookie("loggedIn", false);
-    return response
-      .status(401)
-      .redirect(`${FRONTEND_URL}/login?loggedIn=false`);
-  };
+const isAuthenticated = () => (request: Request, response: Response, next: NextFunction) => {
+  if (request.user) {
+    console.log("REQUEST.USER EXISTS");
+    response.cookie("loggedIn", true);
+    return next();
+  }
+  // if user is not logged in, redirect.
+  return response.status(401).redirect(`${FRONTEND_URL}/login?loggedIn=false`);
+};
 
 export default isAuthenticated;
